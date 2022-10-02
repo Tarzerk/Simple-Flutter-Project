05ad22d8e7eb1272474948b83f8deb712d2a9dad
tarzerk <its.krei@gmail.com>
2022-10-02 16:24:42 -0500

    test

---
+++ lib/Screens/test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Test extends StatefulWidget {
+  const Test({Key? key}) : super(key: key);
+
+  @override
+  State<Test> createState() => _TestState();
+}
+
+class _TestState extends State<Test> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("Hello");
+  }
+}
